c561c2f79355fb08273b302e63e90485b0332d9b
amdk8 <am@inlead.dk>
2015-11-11 13:41:34 +0200

    MOS-1 - Content CRUD. Methods rename to generic names.

--- src/AppBundle/Rest/RestContent.php
+++ src/AppBundle/Rest/RestContent.php
@@ -23,7 +23,7 @@ class RestContent extends RestBaseRequest
         );
     }
 
-    protected function validateContent()
+    protected function validate()
     {
         foreach ($this->requiredFields as $field)
         {
@@ -36,21 +36,16 @@ class RestContent extends RestBaseRequest
                 throw new RestException("Tried to assign content to agency {$this->requestBody['body'][$field]} which does not exist.");
             }
         }
-
-        if ($this->contentExists($this->requestBody['body']['nid'], $this->requestBody['body']['agency']))
-        {
-            throw new RestException("Content with nid {$this->requestBody['body']['nid']}, agency {$this->requestBody['body']['agency']} already exists.");
-        }
     }
 
-    public function contentExists($nid, $agencyId)
+    public function exists($nid, $agencyId)
     {
-        $agency = $this->getContent($nid, $agencyId);
+        $agency = $this->get($nid, $agencyId);
 
         return !is_null($agency);
     }
 
-    public function getContent($nid, $agencyId)
+    public function get($nid, $agencyId)
     {
         $criteria = array(
             'nid' => $nid,
@@ -66,27 +61,52 @@ class RestContent extends RestBaseRequest
 
     public function handleRequest($method)
     {
-        $this->validateContent();
+        $this->validate();
         $requestResult = '';
+
+        $nid = $this->requestBody['body']['nid'];
+        $agency = $this->requestBody['body']['agency'];
 
         switch ($method)
         {
             case 'POST':
+                if (!$this->exists($nid, $agency))
+                {
+                    throw new RestException("Content with nid {$nid}, agency {$agency} does not exist.");
+                }
+                else {
+                    $updatedContent = $this->update($nid, $agency);
+                    $requestResult = 'Updated content with id: ' . $updatedContent->getId();
+                }
                 break;
             case 'PUT':
-                $insertedContent = $this->insertContent();
-                $requestResult = 'Created content with id: ' . $insertedContent->getId();
+                if ($this->exists($nid, $agency))
+                {
+                    throw new RestException("Content with nid {$nid}, agency {$agency} already exists.");
+                }
+                else {
+                    $insertedContent = $this->insert();
+                    $requestResult = 'Created content with id: ' . $insertedContent->getId();
+                }
                 break;
             case 'DELETE':
+                if (!$this->exists($nid, $agency))
+                {
+                    throw new RestException("Content with nid {$nid}, agency {$agency} does not exist.");
+                }
+                else {
+                    $deletedContent = $this->delete($nid, $agency);
+                    $requestResult = 'Deleted content with id: ' . $deletedContent->getId();
+                }
                 break;
         }
 
         return $requestResult;
     }
 
-    private function insertContent()
+    private function insert()
     {
-        $contentObject = $this->prepareContentObject();
+        $contentObject = $this->prepare(new FSContent());
 
         $dm = $this->em->getManager();
         $dm->persist($contentObject);
@@ -95,9 +115,30 @@ class RestContent extends RestBaseRequest
         return $contentObject;
     }
 
-    public function prepareContentObject()
+    private function update($nid, $agencyId)
     {
-        $content = new FSContent();
+        $contentObject = $this->get($nid, $agencyId);
+        $updatedObject = $this->prepare($contentObject);
+
+        $dm = $this->em->getManager();
+        $dm->flush();
+
+        return $updatedObject;
+    }
+
+    private function delete($nid, $agencyId)
+    {
+        $contentObject = $this->get($nid, $agencyId);
+
+        $dm = $this->em->getManager();
+        $dm->remove($contentObject);
+        $dm->flush();
+
+        return $contentObject;
+    }
+
+    public function prepare(FSContent $content)
+    {
         $body = $this->requestBody['body'];
 
         $nid = !empty($body['nid']) ? $body['nid'] : 0;
