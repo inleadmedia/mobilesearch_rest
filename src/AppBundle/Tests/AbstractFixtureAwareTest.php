de32a9177d44da87bc60de31186db606fd995509
AM <am@inlead.dk>
2018-02-22 18:31:19 +0200

    MOS-18 - Test coverage for most cases.

---
+++ src/AppBundle/Tests/AbstractFixtureAwareTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace AppBundle\Tests;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\Executor\MongoDBExecutor;
+use Doctrine\Common\DataFixtures\Loader;
+use Doctrine\Common\DataFixtures\Purger\MongoDBPurger;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+
+abstract class AbstractFixtureAwareTest extends AbstractBaseTest
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        $odm = $this->getContainer()->get('doctrine_mongodb');
+        $em = $odm->getManager();
+
+        $loader = new Loader();
+        /** @var Fixture $fixture */
+        foreach ($this->getFixtures() as $fixture) {
+            if ($fixture instanceof ContainerAwareInterface) {
+                $fixture->setContainer($this->getContainer());
+            }
+            $loader->addFixture($fixture);
+        }
+
+        $purger = new MongoDBPurger($em);
+        $executor = new MongoDBExecutor($em, $purger);
+        $executor->execute($loader->getFixtures());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function tearDown()
+    {
+        parent::tearDown();
+    }
+
+    /**
+     * Provides a list of functions to be loaded prior to test execution.
+     *
+     * @return array
+     */
+    abstract public function getFixtures(): array;
+}
---
+++ src/AppBundle/Tests/TermSuggestionsTest.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace AppBundle\Tests;
+
+use AppBundle\DataFixtures\MongoDB\AgencyFixtures;
+use AppBundle\DataFixtures\MongoDB\ContentFixtures;
+use Symfony\Component\HttpFoundation\Response;
+
+class TermSuggestionsTest extends AbstractFixtureAwareTest
+{
+    use AssertResponseStructureTrait;
+
+    const AGENCY = '999999';
+
+    const URI = '/taxonomy/terms';
+
+    /**
+     * Fetch term suggestions with missing agency.
+     */
+    public function testMissingAgency()
+    {
+        $parameters = [
+            'agency' => '',
+            'vocabulary' => 'field_ding_event_category',
+            'content_type' => 'ding_event',
+            'query' => 'Alpha'
+        ];
+
+        $uri = implode('/', [
+            self::URI,
+            $parameters['vocabulary'],
+            $parameters['content_type'],
+            $parameters['query'],
+        ]);
+
+        /** @var Response $response */
+        $response = $this->request($uri, $parameters, 'GET');
+
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $result = json_decode($response->getContent(), true);
+
+        $this->assertResponseStructure($result);
+        $this->assertFalse($result['status']);
+        $this->assertEmpty($result['items']);
+    }
+
+    /**
+     * Fetches term suggestions.
+     */
+    public function testTermExistence()
+    {
+        $parameters = [
+            'agency' => self::AGENCY,
+            'vocabulary' => 'field_ding_event_category',
+            'content_type' => 'ding_event',
+            'query' => 'Alpha'
+        ];
+
+        $this->assertTermExistence($parameters);
+    }
+
+    /**
+     * Fetches deeply nested term suggestions.
+     */
+    public function testNestedTermExistence()
+    {
+        $parameters = [
+            'agency' => self::AGENCY,
+            'vocabulary' => 'field_ding_event_category',
+            'content_type' => 'ding_event',
+            'query' => 'Theta'
+        ];
+
+        $this->assertTermExistence($parameters);
+    }
+
+    /**
+     * Fetches term suggestions.
+     */
+    public function testTermSuggestions()
+    {
+        $parameters = [
+            'agency' => self::AGENCY,
+            'vocabulary' => 'field_ding_event_category',
+            'content_type' => 'ding_event',
+            'query' => 'a'
+        ];
+
+        $uri = implode('/', [
+            self::URI,
+            $parameters['vocabulary'],
+            $parameters['content_type'],
+            $parameters['query'],
+        ]);
+
+        /** @var Response $response */
+        $response = $this->request($uri, $parameters, 'GET');
+
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $result = json_decode($response->getContent(), true);
+
+        $this->assertResponseStructure($result);
+
+        $terms = $result['items'];
+
+        $this->assertNotEmpty($terms);
+
+        foreach ($terms as $term) {
+            $this->assertContains($parameters['query'], $term, '', true);
+        }
+    }
+
+    /**
+     * Wrapper method to check suggested term existence.
+     *
+     * @param array $parameters     Query parameters.
+     */
+    private function assertTermExistence(array $parameters)
+    {
+        $uri = implode('/', [
+            self::URI,
+            $parameters['vocabulary'],
+            $parameters['content_type'],
+            $parameters['query'],
+        ]);
+
+        /** @var Response $response */
+        $response = $this->request($uri, $parameters, 'GET');
+
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $result = json_decode($response->getContent(), true);
+
+        $this->assertResponseStructure($result);
+
+        $terms = $result['items'];
+        $this->assertCount(1, $terms);
+        $this->assertTrue($terms[0] === $parameters['query']);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFixtures(): array
+    {
+        return [
+            new AgencyFixtures(),
+            new ContentFixtures(),
+        ];
+    }
+}
